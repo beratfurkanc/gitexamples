c1600626fb72d63d551e1558b868b8454c433fa1
beratfurkanc <beratfurkanc@hotmail.com>
2019-08-06 20:16:53 -0400

    Ford Commit

---
+++ src/com/berat/Ford.java
@@ -0,0 +1,10 @@
+package com.berat;
+
+public class Ford {
+
+    public static void main(String[] args) {
+        System.out.println("Ferrari deneme");
+        int a=10;
+        String b="van";
+    }
+}
